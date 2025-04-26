f3766d8a7d9a2a738af20e276ae055b86e440aae
x0reaxeax <xoreaxeax96@gmail.com>
2025-04-26 17:08:52 +0200

    Stripped Gear Artifact

--- Heat-HShifter2/Utils.h
+++ Heat-HShifter2/Utils.h
@@ -37,10 +37,10 @@
 
 #define HEAT_GEAR_ADDRESS_NIBBLE                0x8
 #define HEAT_LAST_GEAR_ADDRESS_NIBBLE           0x0
-#define HEAT_CURRENT_GEAR_ARTIFACT_OFFSET       0x98
+#define HEAT_CURRENT_GEAR_ARTIFACT_OFFSET       0x98            // To be added to artifact
 #define HEAT_LAST_GEAR_ARTIFACT_OFFSET          0x48
 
-#define AOBSCAN_LOW_ADDRESS_LIMIT               0x10000ULL//0x92400000ULL
+#define AOBSCAN_LOW_ADDRESS_LIMIT               0x10000ULL      //0x92400000ULL
 #define AOBSCAN_HIGH_ADDRESS_LIMIT              0x2FFFFFFFFULL
 #define AOBSCAN_SCAN_CHUNK_SIZE                 0x40000U        // 256KB
 
